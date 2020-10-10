5bf8c2d76a82876bd0b8773eeb5648d0a57e120c
GT <gaetan23@msn.com>
2020-10-10 18:48:20 -0400

    initial commit.

---
+++ EfCoreApp.Data/SamuraiContext.cs
@@ -0,0 +1,18 @@
+﻿using EfCoreApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreApp.Data
+{
+    public class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
+            dbContextOptionsBuilder.UseSqlServer(connectionString);            
+        }
+    }
+}
---
+++ EfCoreApp.Domain/Clan.cs
@@ -0,0 +1,8 @@
+﻿namespace EfCoreApp.Domain
+{
+    public class Clan
+    {
+        public int Id { get; set; }
+        public string ClanName { get; set; }
+    }
+}
