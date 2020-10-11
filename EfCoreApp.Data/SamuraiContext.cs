df3f562f84e8a238da23eb4c1caec91eebd67b49
GT <gaetan23@msn.com>
2020-10-10 20:28:20 -0400

    add many to many and one to one.

--- EfCoreApp.Data/SamuraiContext.cs
+++ EfCoreApp.Data/SamuraiContext.cs
@@ -8,11 +8,18 @@ namespace EfCoreApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Battle> Battles { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
             const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
             dbContextOptionsBuilder.UseSqlServer(connectionString);            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+            modelBuilder.Entity<Horse>().ToTable("Horses");
         }
     }
 }
---
+++ EfCoreApp.Domain/SamuraiBattle.cs
@@ -0,0 +1,10 @@
+﻿namespace EfCoreApp.Domain
+{
+    public class SamuraiBattle
+    {
+        public int BattleId { get; set; }
+        public int SamuraiId { get; set; }
+        public Samurai Samurai { get; set; }
+        public Battle Battle { get; set; }
+    }
+}
