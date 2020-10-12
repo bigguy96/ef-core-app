c735cf8ddb98db4ec4808b45ae7420a98910199a
GT <gaetan23@msn.com>
2020-10-12 12:09:31 -0400

    removed console application.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EfCoreApp.Data;
+using EfCoreApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp
 {
@@ -6,6 +10,21 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            var context = new SamuraiContext();
+
+            var samurais = context.Samurais.ToList();
+            var clans = context.Clans.ToList();
+            //var sam = new Samurai
+            //{
+            //    Name = "Joe",
+            //    Clan = new Clan { ClanName = "The Best" },
+            //    Horse = new Horse { Name = "Horsey"},
+            //    Quotes = new List<Quote>() { new Quote { Text ="This is awesome!"} }
+            //};
+
+            //context.Add(sam);
+            //context.SaveChanges();
+            
             Console.WriteLine("Hello World!");
         }
     }
