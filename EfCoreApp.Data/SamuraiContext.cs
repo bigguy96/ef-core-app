9fe47547d1d874c5b17544a76e016171f8c8e512
GT <gaetan23@msn.com>
2020-10-12 10:58:07 -0400

    Added logging.

--- EfCoreApp.Data/SamuraiContext.cs
+++ EfCoreApp.Data/SamuraiContext.cs
@@ -1,5 +1,6 @@
 ﻿using EfCoreApp.Domain;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace EfCoreApp.Data
 {
@@ -10,10 +11,16 @@ namespace EfCoreApp.Data
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
 
+        public static readonly ILoggerFactory LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+        {
+            builder.AddFilter((string category, LogLevel level) =>
+            category == DbLoggerCategory.Database.Name && level == LogLevel.Information).AddConsole();
+        });
+
         protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
         {
             const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
-            dbContextOptionsBuilder.UseSqlServer(connectionString);            
+            dbContextOptionsBuilder.UseSqlServer(connectionString).UseLoggerFactory(LoggerFactory).EnableSensitiveDataLogging();            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
