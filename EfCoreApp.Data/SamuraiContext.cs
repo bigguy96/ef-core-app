7082b523d5c0aaac4f57398f10aebae7a78f9397
GT <gaetan23@msn.com>
2020-10-12 13:45:34 -0400

    Added web api project.

--- EfCoreApp.Data/SamuraiContext.cs
+++ EfCoreApp.Data/SamuraiContext.cs
@@ -1,27 +1,19 @@
 ﻿using EfCoreApp.Domain;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 
 namespace EfCoreApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Clan> Clans { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
-
-        public static readonly ILoggerFactory LoggerFactory = Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
-        {
-            builder.AddFilter((string category, LogLevel level) =>
-            category == DbLoggerCategory.Database.Name && level == LogLevel.Information).AddConsole();
-        });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
-        {
-            const string connectionString = "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData";
-            dbContextOptionsBuilder.UseSqlServer(connectionString).UseLoggerFactory(LoggerFactory).EnableSensitiveDataLogging();            
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
